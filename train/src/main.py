0d1c740401a46f9705576bbfdb89b1d2d2884d19
hitimr <mrh@sinntelligence.ai>
2025-05-20 14:41:00 +0200

    loglevel fix

--- train/src/main.py
+++ train/src/main.py
@@ -3,7 +3,10 @@ import re
 from dotenv import load_dotenv
 from dataclasses import asdict
 
+if "LOGLEVEL" in os.environ:
+    os.environ["LOGLEVEL"] = os.environ["LOGLEVEL"].upper()
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
 import math
 import random
 import threading
